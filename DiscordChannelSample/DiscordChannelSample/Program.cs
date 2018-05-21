9c9fcdf0817bab623dff3bc35fa7104e91ec7451
SynHub <hello@syn.co.in>
2018-05-21 12:46:35 +0530

    Discord Channel
    New sample project for Discord Channel creation

---
+++ DiscordChannelSample/DiscordChannelSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Syn.Bot.Channels.Common;
+using Syn.Bot.Channels.Discord;
+using Syn.Bot.Oscova;
+
+namespace DiscordChannelSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oscovaBot = new OscovaBot();
+
+            //Channel Tester provides OscovaTestDialog.
+            var tester = new ChannelTester(Directory.GetCurrentDirectory());
+            
+            oscovaBot.Dialogs.Add(tester.OscovaTestDialog);
+            oscovaBot.Trainer.StartTraining();
+
+            var discordChannel = new DiscordChannel<OscovaBot>(oscovaBot,"DISCORD_BOT_USER_TOKEN");
+            discordChannel.Start().Wait();
+           
+            Console.WriteLine("Your Oscova Bot is now hosted as a Discord Bot. Press any key to exit.");
+            Console.ReadKey();
+
+            discordChannel.Stop().Wait();
+        }
+    }
+}
